67f08d6ebb7a06ed6188d70631c03d01236de8cc
jvmd <jvmdevelop@gmail.com>
2026-04-13 21:52:03 +0500

    chore(backend): fully restructed backend

---
+++ backend/src/main/java/com/jvmd/lyceum_backend/LyceumBackendApplication.java
@@ -0,0 +1,13 @@
+package com.jvmd.lyceum_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LyceumBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LyceumBackendApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/jvmd/lyceum_backend/LyceumBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jvmd.lyceum_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LyceumBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
