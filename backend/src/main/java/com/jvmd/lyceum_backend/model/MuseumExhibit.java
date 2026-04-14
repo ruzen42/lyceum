1bbe7f6aa796441ba311df715be929e9f3ecfa2b
jvmd <jvmdevelop@gmail.com>
2026-04-14 09:58:28 +0500

    feat: exhibit model

--- backend/src/main/java/com/jvmd/lyceum_backend/model/MuseumExhibit.java
+++ backend/src/main/java/com/jvmd/lyceum_backend/model/MuseumExhibit.java
@@ -12,22 +12,12 @@ import lombok.*;
 @AllArgsConstructor
 @Builder
 @EqualsAndHashCode(onlyExplicitlyIncluded = true)
-public class News {
+public class MuseumExhibit {
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @EqualsAndHashCode.Include
     private Long id;
-
-    @Column(nullable = false)
-    private String title;
-
-    @Column(nullable = false)
-    private String content;
-
-    private String imageUrl;
-
-    @ManyToOne(fetch = FetchType.LAZY)
-    private User user;
 
 
 }
